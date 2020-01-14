6e7d67feb4ab5eae3f53d4e7f5574d2a53a76330
Wai Phyo Han <waiphyohn@gmail.com>
2020-01-14 21:41:38 +0630

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
+    title: `WPH`,
+    name: `Wai Phyo Han`,
     siteUrl: `https://novela.narative.co`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome to WPH, the simplest way to start publishing with Gatsby.`,
       maxWidth: 652,
     },
     social: [
